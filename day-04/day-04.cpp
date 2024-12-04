e53d3e536595bd284dd141d7351a38abff790c4e
jun <83899451+zeichensystem@users.noreply.github.com>
2024-12-04 11:55:33 +0100

    Solve Day 04 Part 2

--- day-04/day-04.cpp
+++ day-04/day-04.cpp
@@ -7,7 +7,7 @@
   
     Solutions: 
         - Part 1: 2414 (Example: 18)
-        - Part 2: 
+        - Part 2: 1871 (Example:  9)
     Notes:  
         - Part 1: 
         - Part 2:
@@ -16,12 +16,12 @@
 using aocutil::Grid;
 using Vec2 = aocutil::Vec2<int>;
 
-int part_one(const std::vector<std::string>& lines)
+int part_one(const std::vector<std::string>& lines, bool part_two = false)
 {
     Grid<char> grid{lines}; 
-    std::vector<Vec2> candidates = grid.find_elem_positions('X');
+    std::vector<Vec2> candidates = grid.find_elem_positions(part_two ? 'A' : 'X');
 
-    const auto match_xmas = [&grid](const Vec2& pos) -> int {
+    const auto match_xmas = [&grid](const Vec2& pos) -> int { // For Part 1. 
         const std::string XMAS_STR = "XMAS";
         std::array<int, 8> direction_matched = {0, 0, 0, 0, 0, 0, 0, 0}; 
 
@@ -37,16 +37,32 @@ int part_one(const std::vector<std::string>& lines)
         return std::count_if(direction_matched.cbegin(), direction_matched.cend(), [&XMAS_STR](int n) {return n == std::ssize(XMAS_STR);});
     };
 
+    const auto match_cross_mas = [&grid](const Vec2& pos) -> int { // For Part 2. 
+        const std::array<std::pair<Vec2, Vec2>, 2> cross_dirs = {std::make_pair(Vec2{1, -1}, Vec2{-1, 1}), std::make_pair(Vec2{-1, -1}, Vec2{1, 1})};
+        for (const auto& dir_pair : cross_dirs) {
+            const auto ch_top = grid.try_get(pos + dir_pair.first); 
+            const auto ch_bottom = grid.try_get(pos + dir_pair.second);
+            if (!ch_top.has_value() || !ch_bottom.has_value()) {
+                return 0; 
+            }
+            const bool mas_cross = (ch_top.value() == 'M' && ch_bottom.value() == 'S') || (ch_top.value() == 'S' && ch_bottom.value() == 'M');
+            if (!mas_cross) {
+                return 0;
+            }
+        }
+        return 1;
+    };
+
     int total_matches = 0; 
     for (const Vec2& pos : candidates) {
-        total_matches += match_xmas(pos);
+        total_matches += part_two ? match_cross_mas(pos) : match_xmas(pos);
     }
     return total_matches;
 }
 
 int part_two(const std::vector<std::string>& lines)
 {
-    return -1; 
+    return part_one(lines, true);
 }
 
 int main(int argc, char* argv[])
