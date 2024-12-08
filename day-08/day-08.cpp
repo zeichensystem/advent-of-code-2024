19d4ba7a60f14be80b065f8055d088881409f5c4
jun <83899451+zeichensystem@users.noreply.github.com>
2024-12-08 13:46:39 +0100

    Solve Day 08 Part 2

--- day-08/day-08.cpp
+++ day-08/day-08.cpp
@@ -7,8 +7,8 @@
     Problem: https://adventofcode.com/2024/day/8
   
     Solutions: 
-        - Part 1: 295 (Example: 14)
-        - Part 2: 
+        - Part 1:  295 (Example: 14)
+        - Part 2: 1034 (Example: 34)
     Notes:  
         - Part 1: 
         - Part 2:
@@ -50,13 +50,46 @@ int part_one(const std::vector<std::string>& lines)
             }
         }
     }
-    
+
     return antinode_positions.size();
 }
 
 int part_two(const std::vector<std::string>& lines)
 {
-    return -1; 
+    const Grid<char> grid {lines};
+    std::unordered_map<char, std::vector<Vec2>> antenna_positions; // {antenna_type_xy -> [pos_1, ..., pos_n]}
+    std::unordered_set<Vec2> antinode_positions;
+
+    grid.foreach([&antenna_positions](const Vec2& pos, char elem) {
+        if (elem == '.') {
+            return;
+        }
+        if (antenna_positions.contains(elem)) {
+            antenna_positions.at(elem).push_back(pos);
+        } else {
+            antenna_positions.insert({elem, std::vector<Vec2>{pos}});
+        }
+    });
+     
+    for (auto const& [antenna_type, positions] : antenna_positions) {
+        const int last_i = positions.size() ? std::size(positions) - 1 : 0;
+        for (int i = 0; i < last_i; ++i) {
+            const Vec2 a = positions.at(i);
+            for (int j = i + 1; j < std::ssize(positions); ++j) {
+                const Vec2 b = positions.at(j);
+                for (int dir : {1, -1}) {
+                    Vec2 candidate = a; 
+                    int n = 1;
+                    while (grid.pos_on_grid(candidate)) {
+                        antinode_positions.insert(candidate);
+                        candidate = a + (n++ * dir) * (a - b);
+                    }
+                } 
+            }
+        }
+    }
+
+    return antinode_positions.size(); 
 }
 
 int main(int argc, char* argv[])
