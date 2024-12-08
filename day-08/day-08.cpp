f5531abf2e1c45fa1dcc606a1b551bc43e65484a
jun <83899451+zeichensystem@users.noreply.github.com>
2024-12-08 14:28:53 +0100

    Prettify Day 08

--- day-08/day-08.cpp
+++ day-08/day-08.cpp
@@ -17,10 +17,10 @@
 using Vec2 = aocutil::Vec2<int>;
 using aocutil::Grid;
 
-int part_one(const std::vector<std::string>& lines)
+int part_one(const std::vector<std::string>& lines, bool is_part_two = false)
 {
     const Grid<char> grid {lines};
-    std::unordered_map<char, std::vector<Vec2>> antenna_positions; // {antenna_type_xy -> [pos_1, ..., pos_n]}
+    std::unordered_map<char, std::vector<Vec2>> antenna_positions; // {antenna_type_xy -> [pos_1, ..., pos_n], ...}
     std::unordered_set<Vec2> antinode_positions;
 
     grid.foreach([&antenna_positions](const Vec2& pos, char elem) {
@@ -33,20 +33,34 @@ int part_one(const std::vector<std::string>& lines)
             antenna_positions.insert({elem, std::vector<Vec2>{pos}});
         }
     });
-     
+    
+    const auto handle_pair_p1 = [&grid, &antinode_positions](const Vec2& a, const Vec2& b) {
+        const Vec2 behind_a = b + 2 * (a - b), 
+                   behind_b = a + 2 * (b - a);
+        for (const Vec2& candidate : {behind_a, behind_b}) {
+            if (grid.pos_on_grid(candidate)) {
+                antinode_positions.insert(candidate);
+            }
+        }
+    };
+    const auto handle_pair_p2 = [&grid, &antinode_positions](const Vec2& a, const Vec2& b) {
+        for (int dir : {1, -1}) {
+            Vec2 candidate = a; 
+            int n = 1;
+            while (grid.pos_on_grid(candidate)) {
+                antinode_positions.insert(candidate);
+                candidate = a + (n++ * dir) * (a - b);
+            }
+        }
+    };
+
     for (auto const& [antenna_type, positions] : antenna_positions) {
         const int last_i = positions.size() ? std::size(positions) - 1 : 0;
         for (int i = 0; i < last_i; ++i) {
             const Vec2 a = positions.at(i);
             for (int j = i + 1; j < std::ssize(positions); ++j) {
                 const Vec2 b = positions.at(j);
-                const Vec2 behind_a = b + 2 * (a - b), 
-                           behind_b = a + 2 * (b - a);
-                for (const Vec2& candidate : {behind_a, behind_b}) {
-                    if (grid.pos_on_grid(candidate)) {
-                        antinode_positions.insert(candidate);
-                    }
-                }
+                !is_part_two ? handle_pair_p1(a, b) : handle_pair_p2(a, b);
             }
         }
     }
@@ -56,40 +70,7 @@ int part_one(const std::vector<std::string>& lines)
 
 int part_two(const std::vector<std::string>& lines)
 {
-    const Grid<char> grid {lines};
-    std::unordered_map<char, std::vector<Vec2>> antenna_positions; // {antenna_type_xy -> [pos_1, ..., pos_n]}
-    std::unordered_set<Vec2> antinode_positions;
-
-    grid.foreach([&antenna_positions](const Vec2& pos, char elem) {
-        if (elem == '.') {
-            return;
-        }
-        if (antenna_positions.contains(elem)) {
-            antenna_positions.at(elem).push_back(pos);
-        } else {
-            antenna_positions.insert({elem, std::vector<Vec2>{pos}});
-        }
-    });
-     
-    for (auto const& [antenna_type, positions] : antenna_positions) {
-        const int last_i = positions.size() ? std::size(positions) - 1 : 0;
-        for (int i = 0; i < last_i; ++i) {
-            const Vec2 a = positions.at(i);
-            for (int j = i + 1; j < std::ssize(positions); ++j) {
-                const Vec2 b = positions.at(j);
-                for (int dir : {1, -1}) {
-                    Vec2 candidate = a; 
-                    int n = 1;
-                    while (grid.pos_on_grid(candidate)) {
-                        antinode_positions.insert(candidate);
-                        candidate = a + (n++ * dir) * (a - b);
-                    }
-                } 
-            }
-        }
-    }
-
-    return antinode_positions.size(); 
+   return part_one(lines, true); 
 }
 
 int main(int argc, char* argv[])
